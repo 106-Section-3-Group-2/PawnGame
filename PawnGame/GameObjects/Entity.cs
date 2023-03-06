020f7be225081d8a70353079060215c0ebdafd2f
Thomas <tberrios9800@gmail.com>
2023-03-06 14:09:59 -0500

    Created entity class and commented game object

--- PawnGame/GameObjects/Entity.cs
+++ PawnGame/GameObjects/Entity.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PawnGame.GameObjects
+﻿namespace PawnGame.GameObjects
 {
-    internal class Entity
+    internal abstract class Entity : GameObject
     {
+        protected bool IsAlive;
+        protected Vector2 Velocity;
+
+        /// <summary>
+        /// Makes a new entity object that is alive and has a velocity of 0
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="hitbox"></param>
+        protected Entity(Texture2D texture, Rectangle hitbox) : base(texture, hitbox)
+        {
+            IsAlive = true;
+            Velocity = Vector2.Zero;
+        }
+
+        /// <summary>
+        /// Should update all values needed for the entety, then move/attack
+        /// </summary>
+        protected abstract void Update();
+        /// <summary>
+        /// Should move the entity based on velocity
+        /// </summary>
+        protected abstract void Move();
+        /// <summary>
+        /// Should cause the entity to attack
+        /// </summary>
+        protected abstract void Attack();
     }
 }
