114160b2b220cddf45cd4eba49677b09cef3bb14
atb5915 <atb5915@rit.edu>
2023-03-06 14:11:28 -0500

    created Level and LevelEditor

---
+++ PawnGame/Level.cs
@@ -0,0 +1,15 @@
+﻿using PawnGame.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnGame
+{
+    internal class Level
+    {
+        private Tile[,] tiles;
+        private Enemy[] enemies;
+    }
+}
---
+++ PawnGame/LevelEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnGame
+{
+    internal class LevelEditor
+    {
+        #region fields
+        private string filePath;
+        #endregion
+
+        public void Draw()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
