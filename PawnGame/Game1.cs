022350ba0537f8edf88bbb19b437a3610d364a59
Troy Hamilton <tdh5759@rit.edu>
2023-03-06 14:49:47 -0500

    Created the button class and started implementing them

--- PawnGame/Game1.cs
+++ PawnGame/Game1.cs
@@ -26,6 +26,10 @@ namespace PawnGame
         private MouseState _currMState;
         private MouseState _prevMState;
 
+        private Vector2 _windowSize;
+
+        // Menu buttons
+
         // This font is temporary
         // Will use for creating menu skeleton
         // for implementation of clicking from menu to game
@@ -37,6 +41,8 @@ namespace PawnGame
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _windowSize = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         }
 
         protected override void Initialize()
@@ -51,6 +57,7 @@ namespace PawnGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            font = this.Content.Load<SpriteFont>("Arial");
         }
 
         protected override void Update(GameTime gameTime)
@@ -98,8 +105,28 @@ namespace PawnGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
+            switch (_gameState)
+            {
+                case GameState.Menu:
+                    Button newGameBtn = new Button(font, "New Game",
+                        new Vector2(_windowSize.X / 2 - font.MeasureString("New Game").X / 2, _windowSize.Y - 100), Color.LightGray);
+                    newGameBtn.Draw(_spriteBatch);
+                    break;
+            }
+            #region Menu Skeleton
 
+            _spriteBatch.DrawString(font, "Load Game",
+                new Vector2(_windowSize.X / 2 - font.MeasureString("Load Game").X / 2, _windowSize.Y - 75),
+                Color.White);
+
+            _spriteBatch.DrawString(font, "Level Editor",
+                new Vector2(_windowSize.X / 2 - font.MeasureString("Level Editor").X / 2, _windowSize.Y - 50), 
+                Color.White);
+            #endregion
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
