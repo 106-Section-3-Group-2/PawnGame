f4b20a4fb92f704507a83115efbcd31ef325c168
atb5915 <atb5915@rit.edu>
2023-03-06 14:24:46 -0500

    changed GameObject Draw() access

--- PawnGame/Level.cs
+++ PawnGame/Level.cs
@@ -1,4 +1,5 @@
 ﻿using PawnGame.GameObjects;
+using PawnGame.GameObjects.Enemies;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,36 @@ using System.Threading.Tasks;
 
 namespace PawnGame
 {
+    /// <summary>
+    /// 
+    /// </summary>
     internal class Level
     {
+        #region fields
         private Tile[,] tiles;
-        private Enemy[] enemies;
+        private Enemy[] enemySpawns;
+        private Vector2 spawnPoint;
+        #endregion
+
+        /// <summary>
+        /// create a level that holds an array of tiles, an array of enemies, and a spawnpoint.
+        /// </summary>
+        /// <param name="tiles"></param>
+        /// <param name="enemies"></param>
+        /// <param name="spawnPoint"></param>
+        public Level(Tile[,] tiles, Enemy[] enemies, Vector2 spawnPoint)
+        {
+            this.tiles = tiles;
+            this.enemySpawns = enemies;
+            this.spawnPoint = spawnPoint;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            foreach(Tile tile in tiles)
+            {
+                tile.Draw(sb);
+            }
+        }
     }
 }
