1499972835a149ae90bc2e7c985db84755623f22
atb5915 <atb5915@rit.edu>
2023-03-06 13:48:54 -0500

    created Tile class

--- PawnGame/GameObjects/Tile.cs
+++ PawnGame/GameObjects/Tile.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace PawnGame.GameObjects
 {
-    internal class Tile
+    /// <summary>
+    /// represents the space of a single square in the game. Can be solid, which prevents entities and players from passing through it.
+    /// </summary>
+    internal class Tile : GameObject
     {
+        /// <summary>
+        /// represents whether the Tile is solid and can trigger collision resolutions
+        /// </summary>
+        public bool Solid { get; set; }
+
+        /// <summary>
+        /// create a new tile, specifying whether it is solid
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="hitbox"></param>
+        /// <param name="solid"></param>
+        public Tile(Texture2D texture, Rectangle hitbox, bool solid) : base(texture, hitbox)
+        {
+            Solid = solid;
+        }
     }
 }
