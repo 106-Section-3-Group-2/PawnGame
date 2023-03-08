4bfaff66b8ca998f11c21a2ff060bfd7adf3a2d1
atb5915 <atb5915@rit.edu>
2023-03-08 14:39:34 -0500

    leveleditor class stuff

--- PawnGame/LevelEditor.cs
+++ PawnGame/LevelEditor.cs
@@ -16,23 +16,100 @@ namespace PawnGame
     {
         #region fields
         private string _filePath;
-        private Tile[] _palette;
+        private Tile[] _paletteTiles;
+        private Enemy[] _paletteEnemies;
+        private GameObject _selected;
         private Level _level;
+        private Vector2 _cameraPosition;
         #endregion
 
+
+        
+
+        /// <summary>
+        /// load the level editor to create a new x*y level
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public LevelEditor(int x, int y)
         {
             _level = new Level(new Tile[x, y], new List<Enemy>(), new Vector2());
+            initialize();
         }
 
-        public void Draw(SpriteBatch sb)
+        /// <summary>
+        /// load the level editor with a file path. If the file is not read, creates a new 8*8 level instead and throws an exception with a relevant message.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public LevelEditor(string filePath)
         {
-
+            try
+            {
+                _level = Level.Read(filePath);
+                _filePath = filePath;
+            }
+            catch(Exception e)
+            {
+                _level = new Level(new Tile[8, 8], new List<Enemy>(), new Vector2());
+                throw e;
+            }
+            initialize();
         }
 
+        /// <summary>
+        /// runs initialization code
+        /// </summary>
+        private void initialize()
+        {
+            _paletteTiles = new Tile[]{
+                //one of each kind of tile here
+            };
+            _paletteEnemies = new Enemy[]{
+                //one of each kind of tile here
+            };
+            _selected = _paletteTiles[0];
+            _cameraPosition = new Vector2(100, 100);
+        }
+
+        /// <summary>
+        /// update the level editor
+        /// </summary>
         public void Update()
         {
+            //check for clicks on tile palette
+            for(int i = 0; i < _paletteTiles.Length; i++)
+            {
+                
+            }
 
+            //check for clicks on enemy palette
+            for (int i = 0; i < _paletteEnemies.Length; i++)
+            {
+
+            }
+
+            //check for clicks on tiles in the level
+            for (int i = 0; i < _level.Tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < _level.Tiles.GetLength(1); j++)
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// draw the level editor
+        /// </summary>
+        /// <param name="sb"></param>
+        public void Draw(SpriteBatch sb)
+        {
+            _level.Draw(sb, _cameraPosition);
+        }
+
+        private void CheckMouseOn(GameObject g)
+        {
+            
         }
     }
 }
