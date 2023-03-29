b871c2491b96295e3b18e4d49a2afe7fc2f4a64b
atb5915 <atb5915@rit.edu>
2023-03-29 14:34:05 -0400

    SAVE AND LOAD WORKS BABYYY

--- PawnGame/Program.cs
+++ PawnGame/Program.cs
@@ -1,3 +1,14 @@
-﻿
-using var game = new PawnGame.Game1();
-game.Run();
+﻿using System;
+
+namespace PawnGame
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using var game = new PawnGame.Game1();
+            game.Run();
+        }
+    }
+}
