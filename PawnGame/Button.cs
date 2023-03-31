6fca91b9942784536fbb51fc3e994b011b6d352c
Thomas <tberrios9800@gmail.com>
2023-03-31 16:47:46 -0400

    Removed unnecessary using statements

--- PawnGame/Button.cs
+++ PawnGame/Button.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace PawnGame
+﻿namespace PawnGame
 {
     /// <summary>
     /// Represents a text box that is able to be clicked
--- PawnGame/Level.cs
+++ PawnGame/Level.cs
@@ -2,9 +2,6 @@
 using PawnGame.GameObjects.Enemies;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 using Newtonsoft.Json;
