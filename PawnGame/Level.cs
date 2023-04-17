e910abeccdeffa487e58bf31492b8b455578a73d
Thomas <tberrios9800@gmail.com>
2023-04-17 15:45:53 -0400

    Added get property for level

--- PawnGame/Level.cs
+++ PawnGame/Level.cs
@@ -4,6 +4,14 @@
     {
         private Room[,] _rooms;
 
+        public Room this[int i, int j]
+        {
+            get
+            {
+                return _rooms[i, j];
+            }
+        }
+
         public Level()
         {
 
