4acdee5fb7500a74a584b1636649ef85df7c5b98
Thomas <tberrios9800@gmail.com>
2023-04-23 21:49:31 -0400

    Locked mouse again

--- PawnGame/VirtualMouse.cs
+++ PawnGame/VirtualMouse.cs
@@ -41,17 +41,12 @@ namespace PawnGame
 
         public void Update(MouseState mouse, int windowWidth, int windowHeight)
         {
-
-
-            //Comment out this region
-            /*
             #region Locking to circle
             Vector2 mouseVector = new Vector2(mouse.X - windowWidth / 2, mouse.Y - windowHeight / 2);
             mouseVector.Normalize();
             mouseVector *= 400;
             Mouse.SetPosition((int)(mouseVector.X + windowWidth / 2), (int)(mouseVector.Y + windowHeight / 2));
             #endregion
-            */
 
             x = mouse.X;
             y = mouse.Y;
